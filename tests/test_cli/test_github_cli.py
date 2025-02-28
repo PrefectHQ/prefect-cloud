3b58fbdb2f53991a1f8feb9d084501e530628d5d
jake@prefect.io <jake@prefect.io>
2025-02-28 02:45:14 -0500

    fix some tests

--- tests/test_cli/test_github_cli.py
+++ tests/test_cli/test_github_cli.py
@@ -18,10 +18,10 @@ def mock_outgoing_calls(monkeypatch):
         return context_manager
 
     monkeypatch.setattr(
-        "prefect_cloud.cli.github.install_github_app_interactively", AsyncMock()
+        "prefect_cloud.cli.code_sources.install_github_app_interactively", AsyncMock()
     )
     monkeypatch.setattr(
-        "prefect_cloud.cli.github.get_prefect_cloud_client", mock_get_client
+        "prefect_cloud.cli.code_sources.get_prefect_cloud_client", mock_get_client
     )
 
     return install_mock, client_mock
@@ -93,8 +93,9 @@ def test_github_ls_no_repositories(mock_outgoing_calls):
         command=["github", "ls"],
         expected_code=1,
         expected_output_contains=[
-            "No repositories found! Configure the Prefect Cloud GitHub integration with",
-            "`prefect-cloud github setup`.",
+            "No repositories found!",
+            "Install the Prefect Cloud GitHub App with `prefect-cloud",
+            "github setup`.",
         ],
     )
 
